aa1798099dd0e2928720ee7b7c8656ec8546acf1
Pankaj Kumar <pankaj3359kr@gmail.com>
2021-12-06 18:45:48 +0530

    Fix Header and add animation to section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import MenuIcon from '@mui/icons-material/Menu';
-
+import CloseIcon from '@mui/icons-material/Close';
  
 function Header() {
+    const [burgerStatus, setBurgerStatus] = useState(false);
+
     return (
         <Container>
             <a href="#">
                 <img src="/images/logo.svg"/>
             </a>
-            <MenuGroup>
-                <div>Model S</div>
-                <div>Model X</div>
-                <div>Model Y</div>
-            </MenuGroup>
+            <Menu>
+                <p><a href="#">Model S</a></p>
+                <p><a href="#">Model 3</a></p>
+                <p><a href="#">Model X</a></p>
+                <p><a href="#">Model Y</a></p>
+            </Menu>
             <RightMenu>
                 <a href="#">Shop</a>
                 <a href="#">Account</a>
-                <MenuIconContainer>
+                <CustomMenu onClick={()=>setBurgerStatus(true)}>
                     <MenuIcon></MenuIcon>
-                </MenuIconContainer>
+                </CustomMenu>
             </RightMenu>
+            <BurgerNav show={burgerStatus}>
+                <CloseWrapper>
+                    <CustomClose onClick={()=>setBurgerStatus(false)}/>
+                </CloseWrapper>
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Used Inventory</a></li>
+                <li><a href="#">Trade-in</a></li>
+                <li><a href="#">Cybertruck</a></li>
+                <li><a href="#">Roadster</a></li>
+            </BurgerNav>
         </Container>
     )
 }
@@ -28,20 +41,31 @@ function Header() {
 export default Header
 
 const Container = styled.div`
-    display: flex;
     min-height: 60px;
+    position: fixed;
+    display: flex;
     align-items: center;
     justify-content: space-between;
     padding: 0 20px;
-    margin-bottom: -60px;
+    top:0;
+    left:0;
+    right:0;
+    z-index: 10;
 `
  
-const MenuGroup = styled.div`
+const Menu = styled.div`
     display: flex;
-    div {
+    align-items: center;
+    justify-content: center;
+    flex: 1;
+    a {
         font-weight: 600;
         padding: 0 10px;
         cursor: pointer;
+        flex-wrap: nowrap;
+    }
+    @media(max-width: 768px){
+        display: none;
     }
 `
 
@@ -50,12 +74,48 @@ const RightMenu = styled.div`
     align-items: center;
     a {
         font-weight: 600;
-        padding-right: 10px;
+        margin: 10px;
+        cursor: pointer;
     }
 `
 
-const MenuIconContainer = styled.div`
+const CustomMenu = styled(MenuIcon)`
     display: flex;
     align-items: center;
     cursor: pointer;
+`
+
+const BurgerNav = styled.div`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    background: white;
+    width: 300px;
+    z-index: 100;
+    list-style: none;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    text-align: start;
+    
+    transform: ${props => props.show ? 'translateX(0)': 'translateX(100%)'};
+    transition: transform 0.2s;
+
+    li {
+        padding: 15px 0;
+        border-bottom: 1px solid rgba(0, 0, 0, .2);
+        a{
+            font-weight: 600;
+        }
+    }
+`
+
+const CustomClose = styled(CloseIcon)`
+    cursor: pointer;
+`
+
+const CloseWrapper = styled.div`
+    display: flex;
+    justify-content: flex-end;
 `
